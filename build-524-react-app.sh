7db0b6e9694c04329afa6696a65ebeb66dfd3c39
Jason <jaygurdesigns@gmail.com>
2018-04-17 10:57:29 -0700

    added node_modules to gitignore and the shell file that jenkins will need to execute

---
+++ build-524-react-app.sh
@@ -0,0 +1,9 @@
+﻿export no_proxy=$no_proxy, 10.22.56.10
+npm config set proxy http://contractorproxywest.northgrum.com:80
+npm config set https-proxy http://contractorproxywest.northgrum.com:80
+rm -r node_modules
+npm install -verbose
+npm run build
+mkdir dist
+cp -R build dist
+tar -cvf dist.tar dist
