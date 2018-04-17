32778a5c34150c13aa3cd166da6647dee61adadb
Jason <jaygurdesigns@gmail.com>
2018-04-17 11:15:49 -0700

    Updated jenkins build script to remove the dist folder before recreating

--- build-524-react-app.sh
+++ build-524-react-app.sh
@@ -4,6 +4,7 @@ npm config set https-proxy http://contractorproxywest.northgrum.com:80
 rm -r node_modules
 npm install -verbose
 npm run build
+rm -rf dist
 mkdir dist
 cp -R build dist
 tar -cvf dist.tar dist
